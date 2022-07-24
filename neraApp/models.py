f8933d0303776b861a39c3af36cbcd2bc173c49f
assia-mad <as.madaniyousfi@esi-sba.dz>
2022-07-24 21:18:01 +0100

    adding date of creation to categorie model

--- neraApp/models.py
+++ neraApp/models.py
@@ -31,6 +31,7 @@ class ProductType( models.Model):
 
 class Categorie(models.Model):
     name = models.CharField(max_length=100 , blank= False , null = False)
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.name
 
